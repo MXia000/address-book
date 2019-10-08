314416690240b7b6459d57624023c105845174f0
Mengting Xia <mxia7498@gmail.com>
2019-10-08 00:20:52 -0400

    adding information to address

--- src/ListAddress/ListAddress.js
+++ src/ListAddress/ListAddress.js
@@ -6,6 +6,15 @@ function ListAddress(props) {
     let addresses = [...props.addresses];
     let listItems = addresses.map((item) =>
         <ListGroup.Item key={item.key}>
-            
+            <Card className="bg-light border rounded">
+                <span key={item.key}
+                    className="text-right"
+                    onClick={props.closer.bind(null,item.key)}>{'\u274e'}</span>
+                    <Card.Body className="text-left">
+                        <h5 >{item.firstname} {item.lastname}</h5>
+                        <p>{item.birthday}</p>
+                        <p>{item.telephone}</p>
+                    </Card.Body>
+            </Card>
         </ListGroup.Item>)
 }
