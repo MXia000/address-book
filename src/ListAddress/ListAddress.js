47f538928c4bcec0a761321871024b47fad7a478
Mengting Xia <mxia7498@gmail.com>
2019-10-08 00:10:23 -0400

    initialize function of address

--- src/ListAddress/ListAddress.js
+++ src/ListAddress/ListAddress.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+
+function ListAddress(props) {
+    let addresses = [...props.addresses];
+    let listItems = addresses.map((item) =>
+        <ListGroup.Item key={item.key}>
+            
+        </ListGroup.Item>)
+}
